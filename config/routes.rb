3a48650877dd8806b71417adce3e02b77e164476
Alfredo <jalfredo.gallegos@gmail.com>
2020-04-08 22:46:40 -0700

    Implements bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :assessments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: 'assessments#new'
+
 end
